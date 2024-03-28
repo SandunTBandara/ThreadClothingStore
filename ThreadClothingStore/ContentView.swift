d81cf7059a0df357dc5c00ceeee009db868f2100
SandunTBandara <devocksandu99@gmail.com>
2024-03-28 16:32:17 +0530

    Initial Commit

---
+++ ThreadClothingStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadClothingStore
+//
+//  Created by Sandun Bandara on 2024-03-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
